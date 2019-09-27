ec9df9970d7b26342ac9b44ad6c9393724430b1b
colonkhizer <alibhai93401@gmail.com>
2019-09-27 20:35:54 +0500

    Created Procfile

--- index.js
+++ index.js
@@ -2,18 +2,51 @@ const express = require("express")
 const app = express()
 const path = require("path")
 var exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser')
+const request = require('request')
 
 const PORT = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+function call_api(fetchApi , lookup) {
+		request('https://cloud.iexapis.com/stable/stock/' + lookup +'/quote?token=pk_faee4573aca54cc5aeb65a006fb39e31' , { json:true } , (err,res,body) => {
+		if(err){
+			return console.log(err)
+		}
+		if(res.statusCode === 200){
+			fetchApi(body);
+		}
+	})
+}
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+// set Handlebar index route
+app.get('/', function (req, res) { call_api(function(api){ 
+			res.render('home', { 
+				stock: api ,
+			}); 
+			console.log(api) 
+		} , "fb")
+	}
+);
 
-app.get('/', function (req, res) {
-    res.render('home' , {
-    	stuff: 'My stuff'
-    });
-});
+/// set Handlebar Post Route
+app.post('/', function (req, res) { call_api(function(api){ 
+			res.render('home', {  
+				stock: api ,
+			}); 
+			console.log(api) 
+		} , req.body.stock_ticker)
+	});
+
+app.get('/about' , (req,res) => {
+	res.render('about')
+}
+)
 
 app.use(express.static(path.join(__dirname , 'public')))
 
